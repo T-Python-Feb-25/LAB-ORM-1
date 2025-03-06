2610040d5dc2904e6a5cd4864e4c90d72313effb
eman rajhi <emyksa1429@gmail.com>
2025-03-06 04:04:20 +0300

    add details publish page

--- Blogger/blog/urls.py
+++ Blogger/blog/urls.py
@@ -5,4 +5,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('add/', views.add_post, name='add_post'),
+    path("details/<blog_id>/", views.details, name="details"),
+
 ]
--- Blogger/blog/views.py
+++ Blogger/blog/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from django.utils import timezone
+from django.http import HttpRequest, HttpResponse
 
 def homepage(request):
     posts = Post.objects.filter(is_published=True).order_by('-published_at')
@@ -20,25 +21,9 @@ def add_post(request):
     return render(request, 'blog/add_post.html')
 
 
-# def add_post(request):
-#     if request.method == 'POST':
-#         title = request.POST.get('title')
-#         content = request.POST.get('content')
-#         is_published = request.POST.get('is_published') == 'on'
-        
-#         # تأكد من استخدام اسم الحقل الصحيح بدلاً من "post"
-#         post_image = request.FILES.get("image")  # استبدل "image" باسم الحقل في النموذج
 
-#         # إنشاء المنشور
-#         post = Post(
-#             title=title,
-#             content=content,
-#             is_published=is_published,
-#             published_at=timezone.now(),
-#             image=post_image  # استخدم الحقل الصحيح
-#         )
-#         post.save()
+def details(request:HttpRequest, blog_id:int):
 
-#         return redirect('blog:homepage')  # تأكد من أن هذا المسار موجود في urls.py
+    post = Post.objects.get(pk=blog_id)
 
-#     return render(request, 'blog/add_post.html')
+    return render(request, 'blog/details.html', {"post" : post})
