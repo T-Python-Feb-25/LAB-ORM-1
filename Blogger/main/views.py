39adb06c2d6f98f95b1e04f92372faee69086302
manaralamri <manaralamre33@gmail.com>
2025-03-08 01:52:18 +0300

    lab

--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -3,5 +3,5 @@ from django.http import HttpRequest, HttpResponse
 from post.models import Post
 # Create your views here.
 def home_view(request:HttpRequest):
-  posts = Post.objects.all()
+  posts = Post.objects.all().order_by("-published_at")[0:4]
   return render(request, 'main/index.html', {"posts":posts})
--- Blogger/post/urls.py
+++ Blogger/post/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
   path('create/',views.create_view,name='create_view'),
   path('detail/<post_id>', views.post_detail_view, name='post_detail_view'),
   path('update/<post_id>', views.post_update_view, name="post_update_view"),
-  path('delete/<post_id>', views.post_delete_view, name="post_delete_view")
+  path('delete/<post_id>', views.post_delete_view, name="post_delete_view"),
+  path('all/', views.all_post_view, name='all_post_view'),
+  path('search/', views.search_post_view, name="search_post_view")
 
 ]
